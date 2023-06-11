bf7d04d08948811ac090b56b8c21047f6b78fd01
gatoguapo <victor._espinoza@hotmail.com>
2023-06-10 22:10:10 -0700

    username, email, password validations

--- Newsletter Project/Services/Validations.cs
+++ Newsletter Project/Services/Validations.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Services
 {
-    class Validations
+    public class Validations
     {
+        public bool emptyTxt(string txt)
+        {
+            return string.IsNullOrEmpty(txt);
+        }
+
+        public bool domainEmailValidation(string email)
+        {
+            string dominiosValidos = @"\.com$|\.org$|\.net$|\.edu$";
+            Regex regex = new Regex(dominiosValidos);
+
+            return regex.IsMatch(email);
+        }
+
+        public bool minLengthPasswordValidated(string password)
+        {
+            return password.Length >= 5;
+        }
     }
 }
