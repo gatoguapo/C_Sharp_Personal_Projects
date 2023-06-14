c0ede3d3a98a0f18bb306155f1e3ab5ae3e47f48
gatoguapo <victor._espinoza@hotmail.com>
2023-06-14 15:06:55 -0700

    method getFigurePath

--- Custom_Components/RJControls/RJButton.cs
+++ Custom_Components/RJControls/RJButton.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 
 namespace Custom_Components.RJControls
 {
-    class RJButton
+    public class RJButton : Button
     {
+        //Fields 
+        private int borderSize = 0;
+        private int borderRadius = 40;
+        private Color borderColor = Color.PaleVioletRed;
+
+        //Constructor
+        public RJButton()
+        {
+            this.FlatStyle = FlatStyle.Flat;
+            this.FlatAppearance.BorderSize = 0;
+            this.Size = new Size(150, 40);
+            this.BackColor = Color.MediumSlateBlue;
+            this.ForeColor = Color.White;
+        }
+
+        //Methods
+        private GraphicsPath getFigurePath(RectangleF rect, float radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddArc(rect.X, rect.Y, radius, radius, 180, 90);
+            path.AddArc(rect.Width-radius, rect.Y, radius, radius, 270, 90);
+            path.AddArc(rect.Width-radius, rect.Height-radius, radius, radius, 0, 90);
+            path.AddArc(rect.X, rect.Height - radius, radius, radius, 90, 90);
+            path.CloseFigure();
+
+            return path;
+        }
     }
 }
