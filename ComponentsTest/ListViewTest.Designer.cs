c9c20731ee9ed1963db7bce0321011cce18a4061
gatoguapo <victor._espinoza@hotmail.com>
2023-06-16 15:57:52 -0700

    ListView test created

--- ComponentsTest/ListViewTest.Designer.cs
+++ ComponentsTest/ListViewTest.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace ComponentsTest
 {
-    partial class Form1
+    partial class ListViewTest
     {
         /// <summary>
         /// Required designer variable.
@@ -32,7 +32,7 @@ namespace ComponentsTest
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "ListViewTest";
         }
 
         #endregion
